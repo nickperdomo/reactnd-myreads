0b670414de8ec77e2c99df76f35d9abbb2863d88
Nick Perdomo <nickperdomo121@gmail.com>
2019-05-15 18:38:09 -0700

    Shelf state in Book updates when select menu option is chosen

---
+++ src/Book.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.bookData = props.bookData;
+    this.state = {
+      shelf: this.bookData.shelf,
+    }
+  }
+
+  updateShelf(newShelf) {
+    this.setState(() => ({
+      shelf: newShelf,
+    }))
+  }
+
+
+
+
+  render() {
+    const bookData = this.bookData;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${bookData.imageLinks.thumbnail})` }}></div>
+          <div className="book-shelf-changer">
+            <select value={this.state.shelf} onChange={(event) => this.updateShelf(event.target.value)}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{bookData.title}</div>
+        <div className="book-authors">{bookData.authors.join(", ")}</div>
+      </div>
+    )
+  }
+}
+
+export default Book;
--- src/BookGrid.js
+++ src/BookGrid.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class BookGrid extends Component {
 
-
   render() {
     const { shelfBooks } = this.props;
-    console.log(shelfBooks);
-    const bookList = shelfBooks.map( book => {
+    // console.log(shelfBooks);
+
+    const bookList = shelfBooks.map( bookData => {
       return (
-        <li key={book.id}>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="move" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors.join(", ")}</div>
-          </div>
+        <li key={bookData.id}>
+          <Book bookData={bookData} />
         </li>
       )
     })
