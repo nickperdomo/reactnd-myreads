19e42e9572e313ed8f56df712e5dbd25ab3ad36e
Nick Perdomo <nickperdomo121@gmail.com>
2019-06-23 20:48:36 -0700

    build: Setup build for Netlify

---
+++ build/precache-manifest.71ae91f76b901b9ce1c669bde78a95ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d82f80df148b4f97c7",
+    "url": "/static/css/main.226ff320.chunk.css"
+  },
+  {
+    "revision": "f0d82f80df148b4f97c7",
+    "url": "/static/js/main.f0d82f80.chunk.js"
+  },
+  {
+    "revision": "07b586f87af76c033955",
+    "url": "/static/js/1.07b586f8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "0cf440b6ab8d77506a1f5d842e4e3a58",
+    "url": "/index.html"
+  }
+];
